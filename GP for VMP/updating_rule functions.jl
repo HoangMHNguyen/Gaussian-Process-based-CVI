0ee0469e5a2d23916381f2cb62cd51aca86bc2be
HoangMHNguyen <h.nguyen.huu.minh.hoang@student.tue.nl>
2021-03-07 11:07:56 +0100

    Update updating_rule functions.jl

--- GP for VMP/updating_rule functions.jl
+++ GP for VMP/updating_rule functions.jl
@@ -4,31 +4,29 @@
 function narparams_mu(p, x)
     """
     p: gamma distribution 
-    x: array of observations
+    x: 1 observation
     """
     mean_p = p.α * p.θ;
     η = Vector{Float64}(undef,2); # initialize the natural parameter vector
 
-    η[1] = mean_p * sum(x);
-    η[2] = - (length(x) * mean_p)/2;
+    η[1] = - mean_p/2;
+    η[2] = mean_p * x;
 
     return η
 end
 
 #function to compute η of message mβ
-function  naparams_beta(p,x)
+function  narparams_beta(p,x)
     """
     p: univariate Gaussian distribution 
-    x: array of observation
+    x: 1 observation
     """
     mean_p = p.μ;
     var_p = p.σ^2
     η = Vector{Float64}(undef,2); # initialize the natural parameter vector
 
-    η[1] = -sum(x.^2) + 2*mean_p*sum(x) - length(x)*(var_p + mean_p^2);
-    η[2] = length(x)/2;
-
+    η[1] = 1/2;
+    η[2] = -1/2*x^2 + x*mean_p - 1/2*(var_p + mean_p^2);
+    
     return η
 end
-
-#### update posterior
